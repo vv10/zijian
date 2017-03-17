d3d5452375f91cce053b9fbec05895593ea8e4db
dhuo14 <dhuo14@163.com>
2017-03-17 19:39:22 +0800

    img channel

--- app/assets/javascripts/channel.js
+++ app/assets/javascripts/channel.js
@@ -1,2 +1,21 @@
 //= require base
 //= require plugins/cube-portfolio/jquery.cubeportfolio.min
+
+//= require pubu/modernizr.custom
+//= require pubu/masonry.pkgd.min
+//= require pubu/imagesloaded
+//= require pubu/classie
+//= require pubu/AnimOnScroll
+
+//= require plugins/viewer.min
+
+$(function() {
+
+	new AnimOnScroll( document.getElementById( 'grid' ), {
+		minDuration : 0.4,
+		maxDuration : 0.7,
+		viewportFactor : 0.2
+	} );
+
+	$('.channel').viewer();
+});
--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 Kaminari.configure do |config|
-  config.default_per_page = 20
+  config.default_per_page = 40
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
